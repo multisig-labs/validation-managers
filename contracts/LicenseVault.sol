2d9f5f091611932f51d95550a293c1d737e8d44b
Johnny Gault <john@multisiglabs.org>
2025-03-25 16:19:41 -0700

    Simplify

--- contracts/LicenseVault.sol
+++ contracts/LicenseVault.sol
@@ -22,44 +22,27 @@ contract LicenseVault is
   using Address for address payable;
   using EnumerableSet for EnumerableSet.UintSet;
 
-  // Define roles
   bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
-  // keccak256(abi.encode(uint256(keccak256("ggp.license-vault.storage")) - 1)) & ~bytes32(uint256(0xff));
-  bytes32 private constant STORAGE_SLOT = 0x2f6a4ce7e0d803b1db7346f89a93540904d483a86cfc404f47564789646c7600;
-
-  // Struct to hold all state variables
-  struct Storage {
-    IERC721 nftContract;
-    // As NFTs are added, we lump them all together and do not track which user had which id
-    // This is the set that are in this contract and not staking
-    EnumerableSet.UintSet unstakedNftIds;
-    // These are the ones that have been xfered out to the staking contract
-    EnumerableSet.UintSet stakedNftIds;
-    // Track how many licenses each user has deposited
-    mapping(address => uint32) userLicenseCount;
-    // Track how many licenses each user has requested to withdraw
-    mapping(address => uint32) userWithdrawalRequests;
-  }
-
-  // Custom errors
-  error LicenseNotAvailableForWithdrawal();
-  error NotEnoughLicenses();
-  error WithdrawalAlreadyRequested();
-  error NoWithdrawalRequest();
+  IERC721 public nftContract;
+  // As NFTs are added, we lump them all together and do not track which user had which id
+  // This is the set that are in this contract and not staking
+  EnumerableSet.UintSet unstakedNftIds;
+  // These are the ones that have been xfered out to the staking contract
+  EnumerableSet.UintSet stakedNftIds;
+  // Track how many licenses each user has deposited
+  mapping(address => uint32) userLicenseCount;
+  // Track how many licenses each user has requested to withdraw
+  mapping(address => uint32) userWithdrawalRequests;
 
   event LicensesDeposited(address depositor, uint32 count);
   event LicensesWithdrawn(address withdrawer, uint32 count);
   event WithdrawalRequested(address withdrawer, uint32 count);
-  // Function to access the storage struct
 
-  function _storage() private pure returns (Storage storage $) {
-    bytes32 slot = STORAGE_SLOT;
-    // solhint-disable-next-line no-inline-assembly
-    assembly {
-      $.slot := slot
-    }
-  }
+  error LicensesNotAvailableForWithdrawal();
+  error NotEnoughLicenses();
+  error WithdrawalAlreadyRequested();
+  error NoWithdrawalRequest();
 
   // Initializer instead of constructor
   function initialize(address _nftContract, address _initialAdmin) external initializer {
@@ -68,90 +51,83 @@ contract LicenseVault is
     __Pausable_init();
     __UUPSUpgradeable_init();
 
-    Storage storage s = _storage();
-    s.nftContract = IERC721(_nftContract);
+    nftContract = IERC721(_nftContract);
 
     _grantRole(DEFAULT_ADMIN_ROLE, _initialAdmin);
 
     _pause();
   }
 
-  function deposit(uint256[] calldata nftIds) external nonReentrant {
-    Storage storage $ = _storage();
-
-    for (uint256 i = 0; i < nftIds.length; i++) {
-      $.nftContract.safeTransferFrom(msg.sender, address(this), nftIds[i]);
-      $.unstakedNftIds.add(nftIds[i]);
-      $.userLicenseCount[msg.sender]++;
+  function deposit(uint256[] calldata _nftIds) external nonReentrant {
+    for (uint256 i = 0; i < _nftIds.length; i++) {
+      nftContract.safeTransferFrom(msg.sender, address(this), _nftIds[i]);
+      unstakedNftIds.add(_nftIds[i]);
+      userLicenseCount[msg.sender]++;
     }
-    emit LicensesDeposited(msg.sender, nftIds.length);
+    emit LicensesDeposited(msg.sender, _nftIds.length);
   }
 
-  function requestWithdrawal(uint32 count) external nonReentrant {
-    Storage storage $ = _storage();
-    if ($.userWithdrawalRequests[msg.sender] != 0) {
+  function requestWithdrawal(uint32 _count) external nonReentrant {
+    if (userWithdrawalRequests[msg.sender] != 0) {
       revert WithdrawalAlreadyRequested();
     }
-    if (count > $.userLicenseCount[msg.sender]) {
+    if (_count > userLicenseCount[msg.sender]) {
       revert NotEnoughLicenses();
     }
 
-    $.userWithdrawalRequests[msg.sender] = count;
-    emit WithdrawalRequested(msg.sender, count);
+    userWithdrawalRequests[msg.sender] = _count;
+    emit WithdrawalRequested(msg.sender, _count);
   }
 
   // Assumes we actually have enough NFTs back from the staking contract
   function withdraw() external nonReentrant {
-    Storage storage $ = _storage();
-    if ($.userWithdrawalRequests[msg.sender] == 0) {
+    if (userWithdrawalRequests[msg.sender] == 0) {
       revert NoWithdrawalRequest();
     }
-    if ($.userWithdrawalRequests[msg.sender] > $.unstakedNftIds.length()) {
-      revert LicenseNotAvailableForWithdrawal();
+    if (userWithdrawalRequests[msg.sender] > unstakedNftIds.length()) {
+      revert LicensesNotAvailableForWithdrawal();
     }
 
     // First collect all NFTs to transfer
-    uint256[] memory nftsToTransfer = new uint256[]($.userWithdrawalRequests[msg.sender]);
-    for (uint256 i = 0; i < $.userWithdrawalRequests[msg.sender]; i++) {
-      uint256 lastIndex = $.unstakedNftIds.length() - 1;
-      uint256 lastNftId = $.unstakedNftIds.at(lastIndex);
-      $.unstakedNftIds.remove(lastNftId);
+    uint256[] memory nftsToTransfer = new uint256[](userWithdrawalRequests[msg.sender]);
+    for (uint256 i = 0; i < userWithdrawalRequests[msg.sender]; i++) {
+      uint256 lastIndex = unstakedNftIds.length() - 1;
+      uint256 lastNftId = unstakedNftIds.at(lastIndex);
+      unstakedNftIds.remove(lastNftId);
       nftsToTransfer[i] = lastNftId;
     }
     // Then do a single batch transfer
     // TODO make sure ethix license contract supports batch transfers
-    $.nftContract.safeBatchTransferFrom(address(this), msg.sender, nftsToTransfer);
+    nftContract.safeBatchTransferFrom(address(this), msg.sender, nftsToTransfer);
 
-    $.userWithdrawalRequests[msg.sender] = 0;
+    userWithdrawalRequests[msg.sender] = 0;
     emit LicensesWithdrawn(msg.sender, nftsToTransfer.length);
   }
 
   // Off-Chain fns
 
-  function withdrawForStaking(uint256 limit) external onlyRole(MANAGER_ROLE) {
-    Storage storage $ = _storage();
-    if (limit > $.unstakedNftIds.length()) {
-      limit = $.unstakedNftIds.length();
+  function withdrawForStaking(uint256 _limit) external onlyRole(MANAGER_ROLE) {
+    if (_limit > unstakedNftIds.length()) {
+      _limit = unstakedNftIds.length();
     }
 
-    uint256[] memory nftsToTransfer = new uint256[](limit);
+    uint256[] memory nftsToTransfer = new uint256[](_limit);
 
-    for (uint256 i = 0; i < limit; i++) {
-      uint256 nftId = $.unstakedNftIds.at(i);
-      $.unstakedNftIds.remove(nftId);
-      $.stakedNftIds.add(nftId);
+    for (uint256 i = 0; i < _limit; i++) {
+      uint256 nftId = unstakedNftIds.at(i);
+      unstakedNftIds.remove(nftId);
+      stakedNftIds.add(nftId);
       nftsToTransfer[i] = nftId;
     }
-    $.nftContract.safeBatchTransferFrom(address(this), msg.sender, nftsToTransfer);
+    nftContract.safeBatchTransferFrom(address(this), msg.sender, nftsToTransfer);
   }
 
-  function depositFromStaking(uint256[] calldata nftIds) external onlyRole(MANAGER_ROLE) {
-    Storage storage $ = _storage();
-    for (uint256 i = 0; i < nftIds.length; i++) {
-      $.stakedNftIds.remove(nftIds[i]);
-      $.unstakedNftIds.add(nftIds[i]);
+  function depositFromStaking(uint256[] calldata _nftIds) external onlyRole(MANAGER_ROLE) {
+    for (uint256 i = 0; i < _nftIds.length; i++) {
+      stakedNftIds.remove(_nftIds[i]);
+      unstakedNftIds.add(_nftIds[i]);
     }
-    $.nftContract.safeBatchTransferFrom(msg.sender, address(this), nftIds);
+    nftContract.safeBatchTransferFrom(msg.sender, address(this), _nftIds);
   }
 
   function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4) {
