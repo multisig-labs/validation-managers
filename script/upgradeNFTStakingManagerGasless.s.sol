db69a1326d1be490439965c8f6b6ecd8fee1e38b
Johnny Gault <john@multisiglabs.org>
2025-05-22 11:10:21 -0700

    Add deploy script

---
+++ script/upgradeNFTStakingManagerGasless.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.25;
+
+import { UUPSUpgradeable } from
+  "@openzeppelin-contracts-upgradeable-5.3.0/proxy/utils/UUPSUpgradeable.sol";
+import { Script } from "forge-std-1.9.6/src/Script.sol";
+import { console } from "forge-std-1.9.6/src/console.sol";
+
+import { NFTStakingManagerGasless } from "../contracts/NFTStakingManagerGasless.sol";
+
+contract UpgradeNFTStakingManagerGasless is Script {
+  address public proxyAddress = vm.envAddress("NFT_STAKING_MANAGER");
+
+  function run() external {
+    vm.startBroadcast();
+
+    address implementation = address(new NFTStakingManagerGasless());
+    console.log("New implementation deployed at ", implementation);
+
+    bytes memory data = "";
+    UUPSUpgradeable(proxyAddress).upgradeToAndCall(implementation, data);
+
+    vm.stopBroadcast();
+  }
+}
