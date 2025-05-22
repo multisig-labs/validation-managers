f88cc533d0a72565af550e269419a281b7384232
memerson <emerson.cloud@gmail.com>
2025-05-22 12:23:11 -0400

    Erc2771 (#49)
    * Use EnumerableSet for operator approvals
    
    This will allow us to get all the approvals for a user from a frontend dashboard. We will have to get all the user addresses and then call this function to get all of their approvals.
    
    * Add owner mapping for validations and delegations (#37)
    
    * add owner mappings for validators and delegators
    
    Add mappings from owner to validations and owner to delegations.
    
    These are created when `initiate` is called and removed when the last rewards are distributed, or when complete validator removal is called and there are no rewards left
    
    * delete comments
    
    * Consistent function namees
    
    * reorder functions and limit function visibility
    
    * more function reordering
    
    * test delegation/validations are properly removed
    
    * Add test for getRewardsMintedForEpoch
    
    * WIP
    
    * ERC2771 Gasless
    
    * Protect setTrustedForwarder
    
    * fix-duplicate-tests
    
    * fix: don't delete validations on complete step
    
    * Add back context_init for clarity
    
    ---------
    
    Co-authored-by: Johnny Gault <john@multisiglabs.org>

--- contracts/NFTStakingManager.sol
+++ contracts/NFTStakingManager.sol
@@ -15,7 +15,6 @@ import { UUPSUpgradeable } from
   "@openzeppelin-contracts-upgradeable-5.3.0/proxy/utils/UUPSUpgradeable.sol";
 import { ContextUpgradeable } from
   "@openzeppelin-contracts-upgradeable-5.3.0/utils/ContextUpgradeable.sol";
-
 import { ValidatorManager } from
   "icm-contracts-2.0.0/contracts/validator-manager/ValidatorManager.sol";
 import { ValidatorMessages } from
@@ -134,9 +133,9 @@ interface INativeMinter {
 /// @author MultisigLabs (https://github.com/multisig-labs/validation-managers)
 contract NFTStakingManager is
   Initializable,
-  UUPSUpgradeable,
   ContextUpgradeable,
-  AccessControlDefaultAdminRulesUpgradeable
+  AccessControlDefaultAdminRulesUpgradeable,
+  UUPSUpgradeable
 {
   ///
   /// LIBRARIES
@@ -271,7 +270,7 @@ contract NFTStakingManager is
   }
 
   /// @notice Initializes NFTStakingManager with necessary parameters and settings
-  function initialize(NFTStakingManagerSettings calldata settings) external initializer {
+  function initialize(NFTStakingManagerSettings calldata settings) public initializer {
     UUPSUpgradeable.__UUPSUpgradeable_init();
     ContextUpgradeable.__Context_init();
     AccessControlDefaultAdminRulesUpgradeable.__AccessControlDefaultAdminRules_init(
