4398a818c014eea735d7c9a567ebd3b4f5318a81
Emerson Cloud <emerson.cloud@gmail.com>
2025-05-30 14:30:11 -0400

    Create UniformNodeLicense contract

---
+++ contracts/tokens/UniformNodeLicense.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+import { NodeLicense, NodeLicenseSettings } from "./NodeLicense.sol";
+
+/**
+ * @title UniformNodeLicense
+ * @notice A specialized NodeLicense where all tokens have identical metadata
+ * @dev Overrides tokenURI to return the same URI for all tokens, useful for
+ *      tokens where visual differences don't matter
+ */
+contract UniformNodeLicense is NodeLicense {
+  /**
+   * @dev Returns the same URI for all tokens instead of baseURI + tokenId
+   * @param tokenId The token ID (must exist)
+   * @return The uniform metadata URI for all tokens
+   */
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    _requireOwned(tokenId);
+    return _baseURI();
+  }
+} 
